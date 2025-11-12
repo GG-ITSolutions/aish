6d47c89437cf174c88044f95ef1de2fd1c3b2d2d
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2025-11-12 01:03:56 +0100

    minor changes

--- src/aish/providers.py
+++ src/aish/providers.py
@@ -5,11 +5,13 @@ from pydantic import SecretStr, Field
 
 class ChatOpenRouter(ChatOpenAI):
     def __init__(self, **kwargs):
-        # Extrahiere base_url vor super().__init__
         base_url = kwargs.pop('base_url', "https://openrouter.ai/api/v1")
         
         super().__init__(
             base_url=base_url,
+            default_headers={
+                'X-Title': 'aish'
+            },
             **kwargs
         )
 
